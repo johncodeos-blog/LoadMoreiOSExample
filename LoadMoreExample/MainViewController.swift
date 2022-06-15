da8846347a491ec9e93c405b1be4b9e5006d74b9
John Codeos <johncodeos@gmail.com>
2022-06-15 16:30:39 +0300

    Fix navigation and status bar color

--- LoadMoreExample/MainViewController.swift
+++ LoadMoreExample/MainViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Fix navigation bar color in iOS 15 and above
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+            appearance.backgroundColor = UIColor(named: "primaryColor")
+            navigationController?.navigationBar.standardAppearance = appearance
+            navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        }
     }
-
 }
